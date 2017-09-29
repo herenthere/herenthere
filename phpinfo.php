e3c1053f77d0b752d1cc0360ef6da5ef025d2a25
Francesco Galletti <bogello@hotmail.com>
2017-09-29 12:17:47 -0400

    test

---
+++ phpinfo.php
@@ -0,0 +1,24 @@
+<HTML>
+<body>
+	hello world
+</body>
+</HTML>
+
+
+<?php
+	//page title
+	include('db.php');
+
+	$conn = new mysqli('10.10.7.165', 'root', 'dgklppmarist');
+
+	if ($conn -> connect_error){
+			die ("Connection failed: " . $conn->connect_error);
+	}
+	$query = 'SELECT * FROM USERS';
+
+echo '<pre>';
+echo 'LOADED EXTENSIONS:<br/>';
+print_r(get_loaded_extensions());
+echo '</pre>';
+echo phpinfo();
+?>
