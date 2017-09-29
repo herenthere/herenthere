cef15745ed9192f0e3385bd3d060247f2ea06bc5
Timbille Kulendi <TiMbIlLe@148-100-144-119.FoxNet.marist.edu>
2017-09-29 15:45:03 -0400

    Test TIm

--- db.php
+++ db.php
@@ -7,4 +7,6 @@ db.php is a PhP file created to simply connect to the
 	//Create connection
 	$conn = mysqli_connect('10.10.7.165','root','dgklppmarist','herenthere');
 	mysqli_set_charset($conn,'utf8');
+
+	//Hi - Tim
 ?>
